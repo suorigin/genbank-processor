53b9b11d1a57e1bc61d3b9a735d9b6cdc1b8bc65
Suhongfeng <1436636379@qq.com>
2025-08-31 22:29:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().splitlines()
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith('#')]
 
 setup(
     name="genbank-processor",
@@ -36,4 +36,3 @@ setup(
     },
     include_package_data=True,
 )
-
