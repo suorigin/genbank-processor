a1a04673ef0f7c646cf39a5f412cd7f0f9a031ad
Suhongfeng <1436636379@qq.com>
2025-09-06 01:34:35 +0800

    Revert "V1.2.0"

--- genbank_processor/__init__.py
+++ genbank_processor/__init__.py
@@ -1,6 +1,6 @@
 # genbank_processor/__init__.py
 from .main import main
 
-__version__ = "1.2.0"  # 更新版本号
-__author__ = "suorigin"
-__email__ = "1436636379@qq.com"
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
