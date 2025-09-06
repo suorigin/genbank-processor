04c5856b42e394ce5b9e1daaf97638aed56298e6
Suhongfeng <1436636379@qq.com>
2025-09-06 14:51:32 +0800

    V1.2.0 (#4)
    * v1.2.0
    
    * Update README.md、mian.py、test.yml and so on

--- genbank_processor/__init__.py
+++ genbank_processor/__init__.py
@@ -1,6 +1,8 @@
 # genbank_processor/__init__.py
 from .main import main
 
-__version__ = "1.0.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
+__version__ = "1.2.0"  # 更新版本号
+__author__ = "suorigin"
+__email__ = "1436636379@qq.com"
+
+
